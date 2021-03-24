6b90700afe69414892722cdb3517725130960784
John Donovan <john@geospark.co.uk>
2021-03-24 13:13:40 +0000

    Password encryption almost working apart from one part which is doing my head in.

--- encrypt_password.py
+++ encrypt_password.py
@@ -9,6 +9,8 @@ bits_table = [
     'G', 'Q', 'Y', '8', 'H', 'R', 'Z', '9'
 ]
 
+deshuffle_table = [5, 11, 2, 14, 16, 9, 4, 0, 12, 17, 3, 7, 13, 1, 15, 10, 6, 8]
+
 
 def encode(input_bytes):
     input_buff = BitArray(input_bytes)
@@ -62,23 +64,47 @@ def desmush_bits(smushed_bits):
     return checksum, scrambled_password
 
 
+def create_checksum_characters(checksum):
+    b = checksum & 0x1f
+    a = checksum >> 5
+    return bits_table[b], bits_table[a]
+
+
+def scramble_password(descrambled_password):
+    scrambled_password = ''
+
+    for idx in range(18):
+        j = deshuffle_table[idx]
+        scrambled_password += descrambled_password[j]
+
+    return scrambled_password
+
+
 def main():
-    params = (10000, 13063, 0)
+    params = (10000, 13063, (0, 0, 88))
+    # params = (13464, 46, (15, 1, 96))
+    year = BitArray(uint=params[2][2] - 88, length=7)
+    month = BitArray(uint=params[2][1], length=4)
+    day = BitArray(uint=params[2][0], length=5)
+    date = year + month + day
     input_bytes = BitArray(uintbe=params[0], length=32)
     input_bytes.append(BitArray(uintbe=params[1], length=32))
-    input_bytes.append(BitArray(uintbe=params[2], length=16))
+    input_bytes.append(date)
     output_buff = encode(input_bytes)
     hashed = hash_serial(params[0])
     smushed_bytes = deswizzle(BitArray(output_buff), hashed)
+    # This is wrong, and I can't figure out why. It's a brainfart, I'm sure.
     checksum, scrambled_password = desmush_bits(BitArray(smushed_bytes))
+    a, b = create_checksum_characters(checksum)
+    scrambled_password.append(a)
+    scrambled_password.append(b)
 
     print(BitArray(input_bytes).hex)
     print(BitArray(output_buff).hex)
     print(BitArray(smushed_bytes).hex)
     print(checksum)
     print(''.join(scrambled_password))
-
-    pass
+    print(scramble_password(scrambled_password))
 
 
 if __name__ == '__main__':
